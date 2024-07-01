13a94239312530fba08406351b3d41c2ed3735ce
qasimroy <qasimroy5234@gmail.com>
2024-07-01 19:08:36 +0500

    auth setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'created_by',
+        'updated_by'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\Authentication\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+Route::post("/auth/register", [AuthController::class, "register"]);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::group(["middleware"=> ['auth:sanctum']],function(){
+    Route::get("/", [AuthController::class, 'index']);
+    Route::get("/auth/logout", [AuthController::class, 'logout']);
+});
 
 Route::apiResource('orders', OrderController::class);
